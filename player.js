a2f96a902d44d01729b671056d78d4ac0900be6b
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-29 15:47:39 -0400

    part 3: added new methods

--- player.js
+++ player.js
@@ -15,3 +15,6 @@ var player = {
     console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
   }
 }; //end player
+// player.feedTamagotchi(tamagotchi1)
+// player.medicateTamagotchi(tamagotchi2)
+// player.knockOutTamagotchi(tamagotchi3)
--- tamagotchi.js
+++ tamagotchi.js
@@ -13,15 +13,28 @@ class Tamagotchi {
     }
     cry() {
         this.foodInTummy--;
-        console.log('new food in tummy:' + this.foodInTummy);
+        console.log('new food in tummy: ' + this.foodInTummy);
         console.log("WAHH!!!")
-
     }
+
+
+    puke() {
+        this.foodInTummy--;
+        console.log('new food in tummy: ' + this.foodInTummy + ' neh')
+    }
+
+    yawn() {
+        this.restedness--;
+        console.log("bob has current restedness of: sleep")
+    }
+
 }
+
 
 //create new Tamagotchis
 let constructedObject1 = new Tamagotchi('bob', 'cry')
 let constructedObject2 = new Tamagotchi('joe', 'play');
+
 
 //console log the new Objects to check them out
 console.log(constructedObject1);
@@ -30,7 +43,9 @@ console.log(constructedObject2);
 //Invoke methods on the constructed objects:
 constructedObject1.cry();
 constructedObject2.cry();
+constructedObject1.puke()
+constructedObject2.yawn()
 
-// console.log(`constructor: name: ${this.name}, creatureType: ${this.creatureType}`)
+
 
 
