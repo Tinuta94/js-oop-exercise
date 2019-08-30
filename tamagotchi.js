52bab3c9bc09f1d8957daf514ea5fb1c818ecf78
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-29 22:07:19 -0400

    tried to solve the 5 problem

--- tamagotchi.js
+++ tamagotchi.js
@@ -7,8 +7,11 @@ class Tamagotchi {
         this.creatureType = creatureType;
         this.foodInTummy = 10;
         this.restedness = 10;
-        this.health = 10
-        console.log(`constructor: name: ${this.name}, creatureTupe: ${this.creatureType}`)
+        this.health = 10;
+        // this.hungerTimer = 6000;
+        // this.yawnTimer = 10000;
+        // this.sickTimer = 20000
+        console.log(`constructor: name: ${this.name}, creatureType: ${this.creatureType}`)
 
     }
     cry() {
@@ -28,23 +31,57 @@ class Tamagotchi {
         console.log("bob has current restedness of: sleep")
     }
 
+    startHungerTimer() {
+        this.hungerTimer = window.setInterval(function(){ 
+            console.log("bob")
+            this.cry();
+        }, 6000)
+        clearInterval(this.hungerTimer)
+        console.log("bob")
+    }
+   
+
+    startYawnTimer() {
+        this.yawnTimer = window.setInterval(function () {
+            this.yawn();
+        }, 10000)
+        console.log("joe")
+    }
+
+    startSickTimer() {
+        this.sickTimer = window.setInterval(function() {
+        this.puke();
+    }, 20000)
+    console.log("charlie")
+}   
 }
 
+//     clearInterval(this.hungerTimer)
+//     console.log("stop");
+// }
 
+
+//         clearInterval(this.hungerTimer)
+//     }
+
+// }
 //create new Tamagotchis
 let constructedObject1 = new Tamagotchi('bob', 'cry')
 let constructedObject2 = new Tamagotchi('joe', 'play');
+let constructedObject3 = new Tamagotchi('charlie', 'eat')
 
 
 //console log the new Objects to check them out
 console.log(constructedObject1);
 console.log(constructedObject2);
+console.log(constructedObject3);
 
 //Invoke methods on the constructed objects:
 constructedObject1.cry();
-constructedObject2.cry();
-constructedObject1.puke()
+constructedObject3.puke();
 constructedObject2.yawn()
+
+
 
 
 
